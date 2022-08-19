1c5093a94ccea565ddafa986d58247f0f2acd71e
Matthew Gerloff <matthewgerloff@Hsin-Lings-MBP.fuel.digital>
2022-08-19 16:04:17 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import AllInOne from "./components/AllInOne";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar />
+        <Hero />
+        <About />
+        <Support />
+        <AllInOne />
+        <Pricing />
+        <Footer />
+    </>
   );
 }
 
